c8b67394b7b8c70c0887c57e1e81a545020a4cb1
Miroslav <m.savin@profsoft.pro>
2022-03-13 23:00:57 +0400

    Initial commit

---
+++ app/src/main/java/com/norsion/renote/feature_note/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.norsion.renote.feature_note
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.norsion.renote.ui.theme.ReNoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ReNoteTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ReNoteTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/norsion/renote/feature_note/domain/model/Note.kt
@@ -0,0 +1,5 @@
+package com.norsion.renote.feature_note.domain.model
+
+data class Note(
+    
+)
